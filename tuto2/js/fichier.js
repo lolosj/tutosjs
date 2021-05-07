cd6bc2c019c6253d9e603cc567d2512cbeb854e5
lolo <adcomere@gmail.com>
2021-05-07 17:09:32 +0700

    tuto2

---
+++ tuto2/js/fichier.js
@@ -0,0 +1,25 @@
+var uneString = "str";
+var unInt = 0;
+var unBool = false;
+var typeDeVar;
+
+alert(typeof typeDeVar);
+
+
+alert(uneString);
+alert(unBool);
+alert(unInt);
+
+typeDeVar = typeof uneString;
+alert(typeDeVar);
+
+typeDeVar = typeof unBool;
+alert(typeDeVar);
+
+typeDeVar = typeof unInt;
+alert(typeDeVar);
+
+alert(typeof typeDeVar);
+
+typeDeVar = null;
+alert(typeof typeDeVar);
